5070d09cffd81e7da5df12f67d269ac59f709314
Vinicius <vavizvini000@gmail.com>
2023-09-18 00:11:21 -0300

    feat: types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
+import { DefaultValidationResponseType } from "./base/types/default-validation-response.type";
 import { ValidationSchema } from "./base/types/validation-schema.type";
 import { ValidatorObject } from "./base/object/validator-object";
 import { DefaultTypes } from "./base/types/default-types";
+import { ErrorType } from "./base/types/default-response-error.type";
 
 namespace vt {
     export type Schema = ValidationSchema;
     export type Types = DefaultTypes;
+    export type Error = ErrorType;
+    export type Response = DefaultValidationResponseType;
     export class Model extends ValidatorObject { };
 }
 
